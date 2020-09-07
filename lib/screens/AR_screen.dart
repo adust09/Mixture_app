a50f197e0ea8f4296a182b03bd0a72e0f43eb669
Nishino0719 <nishino0719@icloud.com>
2020-09-07 16:15:18 +0900

    add AR screen

--- lib/screens/AR_screen.dart
+++ lib/screens/AR_screen.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'package:arkit_plugin/arkit_plugin.dart';
-import 'package:vector_math/vector_math_64.dart';
+import 'package:vector_math/vector_math_64.dart' as vector;
 
 class ARScreen extends StatefulWidget {
   static const routeName = '/ar';
@@ -15,6 +15,7 @@ class ARScreen extends StatefulWidget {
 
 class _ARScreenState extends State<ARScreen> {
   ARKitController arkitController;
+  ARKitSphere sphere;
 
   @override
   void dispose() {
@@ -23,14 +24,50 @@ class _ARScreenState extends State<ARScreen> {
   }
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-      appBar: AppBar(title: const Text('AR Screen')),
-      body: ARKitSceneView(onARKitViewCreated: onARKitViewCreated));
+  Widget build(BuildContext context) =>
+      Scaffold(
+        appBar: AppBar(title: const Text('AR Screen')),
+        body: Container(
+          child: ARKitSceneView(
+            enableTapRecognizer: true,
+            onARKitViewCreated: onARKitViewCreated,
+          ),
+        ),
+      );
 
-  void onARKitViewCreated(ARKitController arkitController) {
-    this.arkitController = arkitController;
+  void onARKitViewCreated(ARKitController arKitController) {
+    this.arkitController = arKitController;
+    this.arkitController.onNodeTap = (nodes) => onNodeTapHandler(nodes);
+
+    final material = ARKitMaterial(
+        diffuse: ARKitMaterialProperty(
+          color: Colors.yellow,
+        ));
+    sphere = ARKitSphere(
+      materials: [material],
+      radius: 0.1,
+    );
+
     final node = ARKitNode(
-        geometry: ARKitSphere(radius: 0.1), position: Vector3(0, 0, -0.5));
+      name: 'sphere',
+      geometry: sphere,
+      position: vector.Vector3(0, 0, -0.5),
+    );
     this.arkitController.add(node);
+  }
+
+  onNodeTapHandler(List<String> nodesList) {
+    final name = nodesList.first;
+    final color = sphere.materials.value.first.diffuse.color == Colors.yellow
+        ? Colors.blue
+        : Colors.yellow;
+    sphere.materials.value = [
+      ARKitMaterial(diffuse: ARKitMaterialProperty(color: color))
+    ];
+    showDialog<void>(
+      context: context,
+      builder: (BuildContext context) =>
+          AlertDialog(content: Text('宝を見つけました！おめでとう！')),
+    );
   }
 }

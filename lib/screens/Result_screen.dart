4e0a657af4e2b3b3cc5b799ae5f2a16ce92bc394
maru0804 <>
2020-09-24 01:43:04 +0900

    画面デザインの変更

--- lib/screens/Result_screen.dart
+++ lib/screens/Result_screen.dart
@@ -1,27 +1,52 @@
 import 'package:flutter/material.dart';
+import 'home_screen.dart';
 
-class ResultScreen extends StatefulWidget{
+class ResultScreen extends StatefulWidget {
   static const routeName = '/result';
 
   @override
   _ResultScreenState createState() => _ResultScreenState();
 }
 
-class _ResultScreenState extends State<ResultScreen>{
+class _ResultScreenState extends State<ResultScreen> {
   @override
-  Widget build(BuildContext context){
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Result Screen"),
+  Widget build(BuildContext context) {
+    return Stack(children: <Widget>[
+      new Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: new BoxDecoration(
+          image: new DecorationImage(
+            image: new AssetImage("images/background_image1.jpg"),
+            fit: BoxFit.cover,
+          ),
         ),
-        body:Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text("This is Result Screen"),
-              ],
-            )
-        )
-    );
+      ),
+      Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Center(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "congratulations!",
+                style: TextStyle(
+                    fontSize: 44.0,
+                    color: Colors.yellowAccent,
+                    shadows: [Shadow(color: Colors.red, blurRadius: 16.0)]),
+              ),
+              RaisedButton.icon(
+                  icon: Icon(
+                    Icons.refresh,
+                    color: Colors.white,
+                  ),
+                  label: Text('ホームに戻る', style: TextStyle(fontSize: 20)),
+                  color: Colors.green,
+                  shape: StadiumBorder(),
+                  onPressed: () => Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => HomeScreen())))
+            ],
+          )))
+    ]);
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'AR_screen.dart';
+import 'Result_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   static const routeName = '/home';
@@ -11,19 +12,56 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Home Screen"),
+    return Stack(children: <Widget>[
+      new Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: new BoxDecoration(
+          image: new DecorationImage(
+            image: new AssetImage("images/background_image.jpg"),
+            fit: BoxFit.cover,
+          ),
         ),
+      ),
+      Scaffold(
+        backgroundColor: Colors.transparent,
+
         body: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            RaisedButton(
-                child: Text('Press This Button', style: TextStyle(fontSize: 20)),
-                onPressed: () => Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (context) => ARScreen())))
-          ],
-        )));
+          child: new Container(
+            color: Colors.transparent,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text("宝探しゲーム",style: TextStyle(
+                      fontSize: 48.0,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.yellowAccent,
+                      shadows: [Shadow(color: Colors.greenAccent,blurRadius: 16.0)]
+                  ),
+                  ),
+                  RaisedButton.icon(
+                      icon: Icon(
+                        Icons.file_upload,
+                        color: Colors.white,
+                      ),
+                      label: Text('宝を隠す',style: TextStyle(fontSize: 20)), color:Colors.lightBlue,shape: StadiumBorder(),
+                      onPressed: () => Navigator.of(context)
+                          .push(MaterialPageRoute(builder: (context) => ResultScreen()))),
+
+                  RaisedButton.icon(
+                      icon: Icon(
+                        Icons.directions_run,
+                        color: Colors.white,
+                      ),
+                      label: Text('宝を探す',style: TextStyle(fontSize: 20)), color:Colors.green,shape: StadiumBorder(),
+                      onPressed: () => Navigator.of(context)
+                          .push(MaterialPageRoute(builder: (context) => ARScreen())))
+                ],
+              ),
+          ),
+
+        ),
+      )
+    ]);
   }
 }

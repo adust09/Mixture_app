5748912779dbd72f6a99b5ad9197136c82a1273b
Nishino0719 <nishino0719@icloud.com>
2020-09-24 01:22:00 +0900

    fix page transition

--- lib/screens/AR_screen.dart
+++ lib/screens/AR_screen.dart
@@ -144,111 +144,114 @@ class _ARScreenState extends State<ARScreen> with SingleTickerProviderStateMixin
   Widget build(BuildContext context) {
     final List<String> accelerometer =
     _accelerometerValues?.map((double v) => v.toStringAsFixed(1))?.toList();
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          if(!_isRecording){
-            start();
-          }
-          if (_isScaledUp) {
-            _animationController.reverse();
-          } else {
-            _animationController.forward();
-          }
-          _isScaledUp = !_isScaledUp;
-        },
-        child: _isRecording ? Icon(Icons.refresh) : Icon(Icons.mic),
-      ),
-      appBar: AppBar(title: const Text('アクションをしろ！')),
-      body: SafeArea(
-          child: new Stack(
-            fit: StackFit.expand,
-            children: <Widget>[
-              Align(
-                child: ARKitSceneView(
-                  detectionImages: const [
-                    ARKitReferenceImage(
-                      name: 'https://avatars0.githubusercontent.com/u/55534054?s=460&u=402783902455ae84995129488dd3a12d0699fd84&v=4',
-                      //ここにfirebaseのCloud storageから取得した画像情報を格納する。→隠し場所
-                      physicalWidth: 0.2,
-                    ),
-                  ],
-                  onARKitViewCreated: onARKitViewCreated,
-                  enableTapRecognizer: true,
-                ),
-              ),
-              anchorWasFound
-                  ? Container()
-                  : Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  '隠したオブジェクトを見つけてね！',
-                  style: Theme
-                      .of(context)
-                      .textTheme
-                      .headline5
-                      .copyWith(color: Colors.white),
-                ),
-              ),
-              Align(
-                child: Column(
-                  children: [
-                    // Padding(
-                    //   child: Row(
-                    //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    //     children: <Widget>[
-                    //       Text('Accelerometer: $accelerometer'),
-                    //     ],
-                    //   ),
-                    //   padding: const EdgeInsets.all(16.0),
-                    // ),
-                    Center(
-                      child: SizeTransition(
-                        axis: Axis.vertical, // default
-                        axisAlignment: 0,
-                        sizeFactor: _animationController
-                            .drive(
-                          CurveTween(curve: Curves.fastOutSlowIn),
-                        )
-                            .drive(
-                          Tween<double>(
-                            begin: 0.1,
-                            end: _countup_accel * 0.01,
-                            // end: _maxDeci*0.01,
-                          ),
-                        ),
-                        child: Column(
-                          children: [
-                            new Wrap(
-                              children: <Widget>[
-                                Image.network(
-                                    "https://avatars0.githubusercontent.com/u/55534054?s=460&u=402783902455ae84995129488dd3a12d0699fd84&v=4",fit: BoxFit.cover
-                                ),
-
-                              ],
-                            ),
-                          ],
-                        ),
+    if(_countup_accel < 10) {
+      return Scaffold(
+        body: SafeArea(
+            child: new Stack(
+              children: <Widget>[
+                Align(
+                  child: ARKitSceneView(
+                    detectionImages: const [
+                      ARKitReferenceImage(
+                        name: 'https://avatars0.githubusercontent.com/u/55534054?s=460&u=402783902455ae84995129488dd3a12d0699fd84&v=4',
+                        //ここにfirebaseのCloud storageから取得した画像情報を格納する。→隠し場所
+                        physicalWidth: 0.2,
                       ),
-                    ),
-                  ],
-                ),
-              ),
-              Positioned.fill(
-                child: BackdropFilter(
-                  filter: ImageFilter.blur(
-                      sigmaX: 10, sigmaY: 10),
-                  child: new Container(
-                    // color: Colors.black.withOpacity(0),
-                    // decoration: BoxDecoration(color: _isRecording ? Colors.white.withOpacity(1-_maxDeci*0.01) : Colors.white.withOpacity(1.0)),
-                    decoration: BoxDecoration(color: _countup_accel > 9 ? Colors.white.withOpacity(0) : Colors.white.withOpacity(1-_countup_opacity*0.1)),
+                    ],
+                    onARKitViewCreated: onARKitViewCreated,
+                    enableTapRecognizer: true,
                   ),
                 ),
-              ),
-            ],
-          )
-      ),
-    );
+                Positioned.fill(
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(
+                        sigmaX: 10, sigmaY: 10),
+                    child: new Container(
+                      // color: Colors.black.withOpacity(0),
+                      // decoration: BoxDecoration(color: _isRecording ? Colors.white.withOpacity(1-_maxDeci*0.01) : Colors.white.withOpacity(1.0)),
+                      decoration: BoxDecoration(color: Colors.white.withOpacity(
+                          1 - _countup_opacity * 0.05)),
+                    ),
+                  ),
+                ),
+              ],
+            )
+        ),
+      );
+    }else{
+      return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            if (!_isRecording) {
+              start();
+            }
+            if (_isScaledUp) {
+              _animationController.reverse();
+            } else {
+              _animationController.forward();
+            }
+            _isScaledUp = !_isScaledUp;
+          },
+          child: _isRecording ? Icon(Icons.refresh) : Icon(Icons.mic),
+        ),
+        appBar: AppBar(title: const Text('アクションをしろ！')),
+        body: SafeArea(
+            child: new Stack(
+              fit: StackFit.expand,
+              children: <Widget>[
+                Align(
+                  child: ARKitSceneView(
+                    detectionImages: const [
+                      ARKitReferenceImage(
+                        name: 'https://avatars0.githubusercontent.com/u/55534054?s=460&u=402783902455ae84995129488dd3a12d0699fd84&v=4',
+                        //ここにfirebaseのCloud storageから取得した画像情報を格納する。→隠し場所
+                        physicalWidth: 0.2,
+                      ),
+                    ],
+                    onARKitViewCreated: onARKitViewCreated,
+                    enableTapRecognizer: true,
+                  ),
+                ),
+                Align(
+                  child: Column(
+                    children: [
+                      Center(
+                        child: SizeTransition(
+                          axis: Axis.vertical, // default
+                          axisAlignment: 0,
+                          sizeFactor: _animationController
+                              .drive(
+                            CurveTween(curve: Curves.fastOutSlowIn),
+                          )
+                              .drive(
+                            Tween<double>(
+                              begin: 0.1,
+                              end: _countup_accel * 0.01,
+                              // end: _maxDeci*0.01,
+                            ),
+                          ),
+                          child: Column(
+                            children: [
+                              new Wrap(
+                                children: <Widget>[
+                                  Image.network(
+                                      "https://avatars0.githubusercontent.com/u/55534054?s=460&u=402783902455ae84995129488dd3a12d0699fd84&v=4",
+                                      fit: BoxFit.cover
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            )
+        ),
+      );
+    }
   }
   void onARKitViewCreated(ARKitController arKitController) {
     this.arkitController = arKitController;
